f89a0d0aa37762cae0a3d525669530c22b0d5164
ashpak.farish@awanoo.com <ashpak.farish@barunitsolutions.com>
2021-12-18 23:58:48 +0530

    Card added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
+import ProductCard from './Components/Card/ProductCard';
 import NavBar from './Components/NavBar/NavBar'
+import LandingImage from './assets/LandingImage.jpg';
+import Categories from './Components/Categories/Categories';
 
 function App() {
   return (
     <div className="App">
-  
      <NavBar />
-      
+     <Categories />
     </div>
   );
 }
